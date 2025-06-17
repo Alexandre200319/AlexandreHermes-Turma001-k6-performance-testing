fcf368a599e11a52925836a6d6f09151192fa5e0
Alexandre Hermes <alexandre1203.ah@gmail.com>
2025-06-17 20:39:47 -0300

    commit2

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,21 +4,26 @@ import { Trend, Rate } from 'k6/metrics';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
+
 export const getActivitiesDuration = new Trend('get_activities_duration');
-export const rateStatusOK = new Rate('rate_status_ok');
+export const successfulRequests = new Rate('successful_requests');
+
 
 export const options = {
   stages: [
-    { duration: '1m', target: 10 },
-    { duration: '2m', target: 150 },
-    { duration: '2m', target: 300 }
+    { duration: '1m', target: 10 }, 
+    { duration: '1m', target: 100 }, 
+    { duration: '1m', target: 200 }, 
+    { duration: '1m', target: 300 }, 
+    { duration: '1m', target: 0 } 
   ],
   thresholds: {
-    get_activities_duration: ['p(95)<5700'],
-    rate_status_ok: ['rate>0.88'],
-    http_req_failed: ['rate<0.12']
+    successful_requests: ['rate>0.88'], 
+    get_activities_duration: ['p(95)<5700'], 
+    http_req_failed: ['rate<0.12'] 
   }
 };
+
 
 export function handleSummary(data) {
   return {
@@ -27,14 +32,16 @@ export function handleSummary(data) {
   };
 }
 
+
 export default function () {
   const url = 'https://fakerestapi.azurewebsites.net/api/v1/Activities';
+
   const res = http.get(url);
 
   getActivitiesDuration.add(res.timings.duration);
-  rateStatusOK.add(res.status === 200);
+  successfulRequests.add(res.status === 200);
 
   check(res, {
-    'Status é 200': () => res.status === 200
+    'Status code is 200': r => r.status === 200
   });
 }
